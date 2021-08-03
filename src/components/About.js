bae8f82fdf2d2093679e4d974e5cbb9572ef603e
hamazumi <khamamoto12@gmail.com>
2021-08-03 09:09:11 -0700

    added contact section

--- src/components/About.js
+++ src/components/About.js
@@ -5,24 +5,24 @@ function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Ken Hamamoto
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            <br className="hidden lg:inline-block" />
+            Full-Stack Software Engineer
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+          I am a software engineer interested in leveraging technology to improve people’s lives. I bring a diverse background of troubleshooting experience and would like to continue building upon it with new tech languages.  
+          Born and raised in Oahu, I believe in bringing a little Aloha in people’s day and would love to be able to use tech to do that. I have previously worked in industries such as marine mammal training, aeronautical mechanics, commercial real estate, and coaching high school volleyball. 
+          I’d like to bring together all these unique techniques to complete complex problems in software. 
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
+              Contact
             </a>
             <a
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
+              Projects
             </a>
           </div>
         </div>
@@ -30,7 +30,7 @@ function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
+            src="./Kualoa.jpeg"
           />
         </div>
       </div>
