43a07958a46626db1bb4f6ebe20e968816aec760
hamazumi <khamamoto12@gmail.com>
2021-08-05 15:22:20 -0700

    changed project title size

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,10 +25,10 @@ function Projects() {
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative w-full border-4 border-gray-800 bg-gray-900">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                  <h2 className="tracking-widest text-base title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                  <h1 className="title-font text-2xl font-medium text-white mb-3">
                     {project.title}
                   </h1>
                   <p className="leading-relaxed">{project.description}</p>
