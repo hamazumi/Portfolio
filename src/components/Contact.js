0a8e5eb5839c24ebe209f0b6f5136e903c4fa473
hamazumi <khamamoto12@gmail.com>
2021-08-06 16:11:51 -0700

    rename pdf

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import { AiFillGithub, AiFillMail, AiFillLinkedin, AiFillFileText } from "react-icons/ai"
-import Pdf from "../docs/SE-Resume.pdf"
+import Pdf from "../docs/SEresume.pdf"
 
 function Contact() {
     return (
