3400fcf2ef66e83844ecbd90854ac2950955f4d6
hamazumi <khamamoto12@gmail.com>
2021-08-10 21:53:08 -0700

    pulse title to show clickable

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,17 +17,17 @@ function Projects() {
               key={project.image}
               target="_blank"
               className="sm:w-1/2 w-100 p-4 ">
-              <div className="flex relative transition duration-500 ease-out transform hover:scale-105 shadow-2xl drop-shadow-xl">
+              <div className="flex relative transition duration-500 ease-out transform hover:scale-105 shadow-2xl drop-shadow-2xl">
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center z-10 opacity-0 hover:opacity-100"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative w-full border-4 border-gray-800 bg-gray-900 shadow-2xl drop-shadow-xl">
+                <div className="px-8 py-10 relative w-full border-4 border-gray-800 bg-gray-900 shadow-2xl drop-shadow-2xl">
                   <h2 className="tracking-widest text-lg title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
-                  <h1 className="title-font text-2xl font-medium text-white mb-3">
+                  <h1 className="title-font text-2xl font-medium text-white mb-3 animate-pulse">
                     {project.title}
                   </h1>
                   <p className="leading-relaxed">{project.description}</p>
